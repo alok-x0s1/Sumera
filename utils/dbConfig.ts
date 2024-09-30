91ccc134458ff3f5282fd62de275902da299f417
Alok Yadav <yalok6321@gmail.com>
2024-09-30 17:05:16 +0530

    Database Config

---
+++ utils/dbConfig.ts
@@ -0,0 +1,10 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
+
+const dbUrl = process.env.NEXT_PUBLIC_DATABASE_URL as string;
+
+const sql = neon(dbUrl);
+const db = drizzle(sql, { schema });
+
+export default db;
---
+++ utils/schema.ts
@@ -0,0 +1,27 @@
+import {
+	pgTable,
+	serial,
+	varchar,
+	timestamp,
+	integer,
+	numeric,
+} from "drizzle-orm/pg-core";
+
+export const Budgets = pgTable("budgets", {
+	id: serial("id").primaryKey(),
+	name: varchar("name").notNull(),
+	amount: integer("amount").notNull(),
+	icon: varchar("icon"),
+	createdBy: varchar("createdBy").notNull(),
+	createdAt: timestamp("createdAt").defaultNow(),
+	updatedAt: timestamp("updatedAt").defaultNow(),
+});
+
+export const Expenses = pgTable("expenses", {
+	id: serial("id").primaryKey(),
+	name: varchar("name").notNull(),
+	amount: integer("amount").notNull().default(0),
+	budgetId: integer("budgetId").references(() => Budgets.id),
+	createdAt: timestamp("createdAt").defaultNow(),
+	updatedAt: timestamp("updatedAt").defaultNow(),
+});
