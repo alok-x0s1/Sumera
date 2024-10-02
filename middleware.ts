447a04461463524d0bea1a567b5fe8a42a7bae0c
Alok Yadav <yalok6321@gmail.com>
2024-10-02 07:42:12 +0530

    Middleware for clerk authentication

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+
+export default clerkMiddleware((auth, req) => {
+	if (isProtectedRoute(req)) auth().protect();
+});
+
+export const config = {
+	matcher: [
+		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+		"/(api|trpc)(.*)",
+	],
+};
